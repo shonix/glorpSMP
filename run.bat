c50a573b9ec17c7192bcad28fe53b5887d2d384d
Peter <peterfut123@gmail.com>
2025-04-08 23:07:44 +0200

    added serverfiles to repo

---
+++ run.bat
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:140a385b00a911d9bc3fd7cd5a1aa012aa79993cc64a14e0bc405d8a161a2837
+size 366
---
+++ run.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:460d5d8b907bd467206e4bbcf35dba5972f56b819dc6a8a00d71144e8b6e5418
+size 365
